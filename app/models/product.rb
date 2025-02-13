a1f1adae6b0e3eb0fe4f89660eba89ef5b04f339
Daire Finn <dmanfinn@gmail.com>
2025-02-13 22:43:18 +0000

    Added image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_one_attached :featured_image # Product has a featured image attached
   has_rich_text :description # Product has a description which is a rich text field
   validates :name, presence: true # Product must have a name
 end
