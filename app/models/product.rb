031b726e3528411b30c51e76171c068d034bcadc
Daire Finn <dmanfinn@gmail.com>
2025-02-13 22:38:44 +0000

    Rich text fields added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
+  has_rich_text :description # Product has a description which is a rich text field
   validates :name, presence: true # Product must have a name
 end
